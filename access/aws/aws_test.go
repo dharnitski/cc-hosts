a37fcecf82160bb07a68d7ee48959c0baa0ae0dd
Dmitry Harnitski <dharnitski@gmail.com>
2025-05-19 16:05:12 -0400

    TestS3GetterAll

--- access/aws/aws_test.go
+++ access/aws/aws_test.go
@@ -20,8 +20,9 @@ import (
 // Verify that S3Getter implements Getter interface.
 var _ access.Getter = (*aws.S3Getter)(nil)
 
-func newAwsConfig(t *testing.T) aws_sdk.Config {
-	t.Helper()
+func TestS3Getter(t *testing.T) {
+	t.Parallel()
+
 	awsConfig, err := config.LoadDefaultConfig(t.Context())
 	require.NoError(t, err)
 
@@ -51,17 +52,49 @@ func newAwsConfig(t *testing.T) aws_sdk.Config {
 		return stack.Initialize.Add(awsMock, middleware.Before)
 	})
 
-	return awsConfig
-}
-
-func TestS3Getter(t *testing.T) {
-	t.Parallel()
-
-	awsConfig := newAwsConfig(t)
-
 	s3Getter := aws.New(awsConfig, "test-bucket", "test-folder")
 
 	buffer, err := s3Getter.Get(t.Context(), "test-file", 5, 10)
+	require.NoError(t, err)
+
+	assert.Equal(t, "1234567890", string(buffer))
+}
+
+func TestS3GetterAll(t *testing.T) {
+	t.Parallel()
+
+	awsConfig, err := config.LoadDefaultConfig(t.Context())
+	require.NoError(t, err)
+
+	awsMock := middleware.InitializeMiddlewareFunc("mock", func(
+		ctx context.Context, in middleware.InitializeInput, next middleware.InitializeHandler,
+	) (
+		middleware.InitializeOutput, middleware.Metadata, error,
+	) {
+		switch v := in.Parameters.(type) {
+		case *s3.GetObjectInput:
+			assert.Equal(t, "test-folder/test-file", *v.Key)
+			assert.Equal(t, "test-bucket", *v.Bucket)
+			assert.Nil(t, v.Range)
+
+			return middleware.InitializeOutput{
+				Result: &s3.GetObjectOutput{
+					Body:          io.NopCloser(bytes.NewReader([]byte("1234567890"))),
+					ContentLength: aws_sdk.Int64(10),
+				},
+			}, middleware.Metadata{}, nil
+		default:
+			panic(fmt.Sprintf("no support for %T", v))
+		}
+	})
+
+	awsConfig.APIOptions = append(awsConfig.APIOptions, func(stack *middleware.Stack) error {
+		return stack.Initialize.Add(awsMock, middleware.Before)
+	})
+
+	s3Getter := aws.New(awsConfig, "test-bucket", "test-folder")
+
+	buffer, err := s3Getter.Get(t.Context(), "test-file", 0, 0)
 	require.NoError(t, err)
 
 	assert.Equal(t, "1234567890", string(buffer))
